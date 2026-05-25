5e83223da7653b6a044e51c003cdbf5d5063904b
Krish-Pandey2007 <pandeykrish252007@gmail.com>
2026-05-25 15:44:04 +0530

    Create sub_optimal.cpp

---
+++ hard_version/src/sub_optimal.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define INF INT_MAX
+
+void solve() {
+    int N, K, L, M;
+    cin >> N >> K >> L >> M;
+
+    vector<int> A(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    int low = 1;
+    int high = 1e8;
+    int ans;
+
+    vector<vector<int>> best_dp(N + 1, vector<int>(K + 1, INF));
+    vector<int> best_cost(N+1);
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        vector<int> b(N);
+
+        for (int i = 0; i < N; i++) {
+            if (A[i] >= mid)
+                b[i] = +1;
+            else
+                b[i] = -1;
+
+            if (i != 0)
+                b[i] = b[i - 1] + b[i];
+        }
+
+        vector<int> cost(N + 1);
+
+        for (int i = L - 1; i < N; i++) {
+            int sum;
+
+            if (i != L - 1)
+                sum = b[i] - b[i - L];
+            else
+                sum = b[i];
+
+            if (L % 2 == 0) {
+                if (sum <= 0)
+                    cost[i + 1] = (2 - sum) / 2;
+                else
+                    cost[i + 1] = 0;
+            } 
+            else {
+                if (sum < 0)
+                    cost[i + 1] = (1 - sum) / 2;
+                else
+                    cost[i + 1] = 0;
+            }
+        }
+
+        vector<vector<int>> dp(N+1,vector<int>(K+1,INF));
+
+        for (int i = 0; i <= N; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                dp[i][j] = dp[i - 1][j];
+
+                if (i >= L && dp[i - L][j - 1] != INF) {
+                    dp[i][j] = min( dp[i][j] ,
+                        dp[i - L][j - 1] + cost[i]);
+                }
+            }
+        }
+
+        if (dp[N][K] <= M) {
+            low = mid + 1;
+            ans = mid;
+            best_dp=dp;
+            best_cost=cost;
+        } 
+        else {
+            high = mid - 1;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
